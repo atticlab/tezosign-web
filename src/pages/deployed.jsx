9fc2aad6932f58b70ca4f64dcf17b8b64135ca9b
Mikle25 <mikle2593@gmail.com>
2021-02-19 11:55:07 +0200

    feat(deployed): add transactionHash link
    feat(deployed): redirect to deployed after Deploy

--- src/pages/deployed.jsx
+++ src/pages/deployed.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Redirect, useLocation } from 'react-router-dom';
 import BtnBack from '../components/BtnBack';
 import Text from '../components/styled/Text';
 
@@ -8,6 +9,12 @@ const DeployedStyle = styled.div`
 `;
 
 const Deployed = () => {
+  const location = useLocation();
+
+  if (!location.state) {
+    return <Redirect to="/not-found" />;
+  }
+
   return (
     <>
       <BtnBack pageName="Create a New Multisig" />
@@ -16,6 +23,16 @@ const Deployed = () => {
         <Text>
           Your transaction has been sent. Please wait until it is added to the
           network, it may take a few minutes.
+        </Text>
+        <Text>
+          Transaction hash:{' '}
+          <a
+            href={`https://delphinet.tezblock.io/transaction/${location.state.transactionHash}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {location.state.transactionHash}
+          </a>
         </Text>
       </DeployedStyle>
     </>
