3d99c8b2afc6fe07c58c87e66f3550da930c6868
shotgunofdeath <faust.child@gmail.com>
2021-02-24 21:22:35 +0200

    feat(owners): cache validations

--- src/components/create-multisig/Owners.jsx
+++ src/components/create-multisig/Owners.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import {
   Form as BForm,
@@ -35,46 +35,68 @@ Yup.addMethod(Yup.string, 'bs58OrHexCheck', function (message) {
   });
 });
 
-let revealed = true;
-const schema = Yup.object().shape({
-  entities: Yup.array()
-    .of(
-      Yup.object().shape({
-        id: Yup.number(),
-        value: Yup.string().when('isPubKey', {
-          is: false,
-          then: Yup.string()
-            .trim()
-            .required('Required')
-            .matches(
-              'tz1|tz2|tz3',
-              'Tezos address must start with tz1, tz2 or tz3',
-            )
-            .matches(/^\S+$/, 'No spaces are allowed')
-            .matches(/^[a-km-zA-HJ-NP-Z1-9]+$/, 'Invalid Tezos address')
-            .length(36, 'Tezos address must be 36 characters long')
-            .test('bs58check', 'Invalid checksum', (val) => bs58Validation(val))
-            .test('sad', 'Address is unrevealed', async () => revealed),
-          otherwise: Yup.string()
-            .required('Required')
-            .matches(/^\S+$/, 'No spaces are allowed')
-            .specificLength(
-              'The string must be either 54-55 (base58) or 64(hex) characters long',
-            )
-            .bs58OrHexCheck('The string must be base58 or hex format'),
-        }),
-        isPubKey: Yup.boolean(),
-      }),
-    )
-    .ensure()
-    .required('Must have addresses')
-    .min(1, 'Minimum of 1 addresses')
-    .max(20, 'Maximum of 20 addresses')
-    .unique('Addresses must be unique', (entity) => entity.value),
-});
+const cacheTest = (asyncValidate) => {
+  let valid = false;
+  let valueCached = '';
+
+  return async (value) => {
+    if (value !== valueCached) {
+      const response = await asyncValidate(value);
+      valueCached = value;
+      valid = response;
+      return response;
+    }
+    return valid;
+  };
+};
 
 const Owners = ({ onSubmit }) => {
   const { isAddressRevealed } = useAPI();
+  const testIsAddressRevealed = async (val) => {
+    if (!val || val.length !== 36 || !bs58Validation(val)) return false;
+    const resp = await isAddressRevealed(val);
+    return resp.data.revealed;
+  };
+  const testAddress = useRef(cacheTest(testIsAddressRevealed));
+
+  const schema = Yup.object().shape({
+    entities: Yup.array()
+      .of(
+        Yup.object().shape({
+          id: Yup.number(),
+          value: Yup.string().when('isPubKey', {
+            is: false,
+            then: Yup.string()
+              .trim()
+              .required('Required')
+              .matches(
+                'tz1|tz2|tz3',
+                'Tezos address must start with tz1, tz2 or tz3',
+              )
+              .matches(/^\S+$/, 'No spaces are allowed')
+              .matches(/^[a-km-zA-HJ-NP-Z1-9]+$/, 'Invalid Tezos address')
+              .length(36, 'Tezos address must be 36 characters long')
+              .test('bs58check', 'Invalid checksum', (val) =>
+                bs58Validation(val),
+              )
+              .test('sad', 'Address is unrevealed', testAddress.current),
+            otherwise: Yup.string()
+              .required('Required')
+              .matches(/^\S+$/, 'No spaces are allowed')
+              .specificLength(
+                'The string must be either 54-55 (base58) or 64(hex) characters long',
+              )
+              .bs58OrHexCheck('The string must be base58 or hex format'),
+          }),
+          isPubKey: Yup.boolean(),
+        }),
+      )
+      .ensure()
+      .required('Must have addresses')
+      .min(1, 'Minimum of 1 addresses')
+      .max(20, 'Maximum of 20 addresses')
+      .unique('Addresses must be unique', (entity) => entity.value),
+  });
 
   return (
     <Card.Body>
@@ -98,7 +120,6 @@ const Owners = ({ onSubmit }) => {
       >
         {({
           setFieldValue,
-          handleBlur,
           isSubmitting,
           values,
           errors,
@@ -152,21 +173,6 @@ const Owners = ({ onSubmit }) => {
                             touched.entities[index].value
                           }
                           style={{ maxWidth: '500px' }}
-                          onBlur={(e) => {
-                            handleBlur(e);
-                            if (
-                              e.target.value &&
-                              e.target.value.length === 36 &&
-                              bs58Validation(e.target.value) &&
-                              !entity.isPubKey
-                            )
-                              isAddressRevealed(e.target.value).then(
-                                (response) => {
-                                  revealed = response.data.revealed;
-                                  validateForm();
-                                },
-                              );
-                          }}
                         />
                         <InputGroup.Append>
                           <OverlayTrigger
